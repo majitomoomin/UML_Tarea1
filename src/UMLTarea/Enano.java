582b561b6b88fb2ab1d1bd707e06876976c8d5cd
majitomoomin <mariajoseportill2@gmail.com>
2024-07-20 20:58:31 -0600

    humano y enano

---
+++ src/UMLTarea/Enano.java
@@ -0,0 +1,16 @@
+package UMLTarea;
+
+public class Enano extends Personaje{
+    private Heroe heroeImplementado;
+    private int Altura;
+
+    public static void Salto(int Altura){}
+
+
+    public void HeroeImplementacion( Heroe heroe){
+        this.heroeImplementado = heroe;
+    }
+    public Heroe getHeroeImplementado(){
+        return getHeroeImplementado();
+    }
+}
---
+++ src/UMLTarea/Humano.java
@@ -0,0 +1,16 @@
+package UMLTarea;
+
+public class Humano extends Personaje{
+    private Heroe heroeImplementado;
+    private int edad;
+
+    public static void Superpoder(){}
+
+
+    public void HeroeImplementacion( Heroe heroe){
+        this.heroeImplementado = heroe;
+    }
+    public Heroe getHeroeImplementado(){
+        return getHeroeImplementado();
+    }
+}
