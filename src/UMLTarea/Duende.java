b1db1533a3c232f59a0496c42e948c9ae9ef9d19
majitomoomin <mariajoseportill2@gmail.com>
2024-07-20 21:12:05 -0600

    Duende y ogro

---
+++ src/UMLTarea/Duende.java
@@ -0,0 +1,15 @@
+package UMLTarea;
+
+public class Duende extends Personaje{
+    private Villano villanoImplementado;
+    private String TipodeDuende;
+
+    public static void AtaqueTerrestre(){}
+
+    public void VillanoImplementacion(Villano villano){
+        this.villanoImplementado = villano;
+    }
+    public Villano getVillanoImplementado(){
+        return getVillanoImplementado();
+    }
+}
---
+++ src/UMLTarea/Ogro.java
@@ -0,0 +1,16 @@
+package UMLTarea;
+
+public class Ogro extends Personaje {
+    private Villano villanoImplementado;
+    private String DirecciondePantano;
+
+    public static void AtaquedeFango(){}
+
+
+    public void VillanoImplementacion( Villano villano){
+        this.villanoImplementado = villano;
+    }
+    public Villano getVillanoImplementado(){
+        return getVillanoImplementado();
+    }
+}
